0de8d6b478a5fd970c2815bcf3b736cade2fc5a4
Kyle Matinzi (Java | Spring | SQL) <kylematinzi@gmail.com>
2025-03-23 19:04:52 -0700

    Initial Commit

---
+++ src/main/java/com/personaltraining/personaltrainingapplication/PersonalTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.personaltraining.personaltrainingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personaltraining/personaltrainingapplication/PersonalTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personaltraining.personaltrainingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
